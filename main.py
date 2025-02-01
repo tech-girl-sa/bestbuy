fc181ee07042328e0004f7c200c3fee3a23f7897
sarramabrouk <mabrouk.sarra3@gmail.com>
2025-02-01 05:20:21 +0100

    create Store class

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from product import Product
+from store import Store
 
 
 def main():
@@ -14,6 +15,16 @@ def main():
 
     bose.set_quantity(1000)
     print(bose.show())
+    product_list = [Product("MacBook Air M2", price=1450, quantity=100),
+                    Product("Bose QuietComfort Earbuds", price=250, quantity=500),
+                    Product("Google Pixel 7", price=500, quantity=250),
+                    ]
+    store = Store(product_list)
+    products = store.get_all_products()
+    print(store.get_total_quantity())
+    print(store.order([(products[0], 1), (products[1], 2)]))
+
+
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
